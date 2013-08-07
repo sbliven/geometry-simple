86adcdefc8df8c1ddcf15fa6d6e50b83bf7b0f2a
Spencer Bliven <sbliven@ucsd.edu>
2013-08-07 13:17:10 -0700

    Converted tests to unittest framework

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 """Geometry-simple Tests
 
 http://code.google.com/p/geometry-simple/
@@ -29,64 +29,62 @@ Created by uekstrom
 
 from geo import *
 from math import *
+import unittest
 
-def checkf(value,correct_value,threshold = 1e-14):
-    if abs(value - correct_value) > threshold:
-        raise RuntimeError("Expected %g, got %g" % (correct_value,value))
-    else:
-        print "ok",
 
-print "Testing:",
+class GeoTest(unittest.TestCase):
 
-# Test construction and measurements
+    def setUp(self):
+        self.origin = Point(0,0,0)
+        self.xaxis = Line(self.origin,Point(1,0,0))
+        self.yaxis = Line(self.origin,Point(0,1,0))
+        self.zaxis = Line(self.origin,Point(0,0,1))
+        self.p1 = Point(1,1,1)
+        self.p2 = Point(1,0,0)
+        self.l = Line(self.p1,self.p2)
+        self.xy_plane = Plane(self.origin,Point(1,0,0),Point(0,1,0))
 
-origin = Point(0,0,0)
-xaxis = Line(origin,Point(1,0,0))
-yaxis = Line(origin,Point(0,1,0))
-zaxis = Line(origin,Point(0,0,1))
-xy_plane = Plane(origin,Point(1,0,0),Point(0,1,0))
+    def test_measurements(self):
+        """ Test construction and measurements """
 
-p1 = Point(1,1,1)
-p2 = Point(1,0,0)
-checkf(p1.distance_to(p2),sqrt(2))
+        self.assertAlmostEqual(self.p1.distance_to(self.p2),sqrt(2))
 
-L = Line(p1,p2)
-checkf(L.distance_to(p1),0)
+        self.assertAlmostEqual(self.l.distance_to(self.p1),0)
 
-checkf(p1.distance_to(xy_plane),1)
+        self.assertAlmostEqual(self.p1.distance_to(self.xy_plane),1)
 
-checkf(L.angle_to(xy_plane),pi/4)
+        self.assertAlmostEqual(self.l.angle_to(self.xy_plane),pi/4)
 
-# Make sure that the fitted line coincides with L for three points on L.
-Lfit = Line(p1,p2,Point(1,0.5,0.5))
-checkf(L.angle_to(Lfit),0)
-checkf(L.distance_to(Lfit),0)
+    def test_lfit(self):
+        """ Make sure that the fitted line coincides with l for three points on l. """
+        Lfit = Line(self.p1,self.p2,Point(1,0.5,0.5))
+        self.assertAlmostEqual(self.l.angle_to(Lfit),0)
+        self.assertAlmostEqual(self.l.distance_to(Lfit),0)
 
-# Midpoints
-p = Point(-1,-2,-3).midpoint_to(Point(1,2,3))
-checkf(p.distance_to(origin),0)
+        """ Midpoints """
+        p = Point(-1,-2,-3).midpoint_to(Point(1,2,3))
+        self.assertAlmostEqual(p.distance_to(self.origin),0)
 
-p = xaxis.midpoint_to(yaxis)
-checkf(p.distance_to(origin),0)
+        p = self.xaxis.midpoint_to(self.yaxis)
+        self.assertAlmostEqual(p.distance_to(self.origin),0)
 
-p = zaxis.midpoint_to(xy_plane)
-checkf(p.distance_to(origin),0)
-
+        p = self.zaxis.midpoint_to(self.xy_plane)
+        self.assertAlmostEqual(p.distance_to(self.origin),0)
 
 
 
-#test movements
+    def test_movements(self):
+        """ test movements """
 
-# pure translation
-mt = Movement(origin,p1)
-checkf(p1.moved(mt).distance_to(origin),2*p1.distance_to(origin))
+        # pure translation
+        mt = Movement(self.origin,self.p1)
+        self.assertAlmostEqual(self.p1.moved(mt).distance_to(self.origin),2*self.p1.distance_to(self.origin))
 
-#pure rotation
-mr = Movement(xaxis,yaxis) # rotate xaxis onto yaxis
-checkf(xaxis.moved(mr).distance_to(yaxis),0)
-checkf(origin.moved(mr).distance_to(origin),0)
-
-
-print "done."
+        #pure rotation
+        mr = Movement(self.xaxis,self.yaxis) # rotate self.xaxis onto self.yaxis
+        self.assertAlmostEqual(self.xaxis.moved(mr).distance_to(self.yaxis),0)
+        self.assertAlmostEqual(self.origin.moved(mr).distance_to(self.origin),0)
 
 
+if __name__ == '__main__':
+    unittest.main()
