562d27dda09dfe4b7c62023d2c1e46450ec129b0
Spencer Bliven <sbliven@ucsd.edu>
2013-08-09 14:42:21 -0700

    Releasing v0.2

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ from distutils.core import setup
 with open('README') as readme:
     long_description = readme.read()
 
-setup(name='geometry-simple',
-    version='0.2',
+name = 'geometry-simple'
+version = '0.2'
+setup(name=name,
+    version=version,
     description='3D geometry library for python',
     long_description=long_description,
     author='Spencer Bliven',
     author_email='spencer@bliven.us',
     url='https://github.com/sbliven/geometry-simple',
-    download_url='https://github.com/sbliven/geometry-simple/dist/',
+    download_url="https://github.com/sbliven/geometry-simple/dist/%s-%s.tar.gz"%(name,version),
     py_modules=['geo'],
     requires=['numpy'],
     license='MIT',
