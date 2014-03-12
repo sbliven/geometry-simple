f8652918c189d0f6327a06550c3721011b4281d5
Spencer Bliven <sbliven@ucsd.edu>
2014-03-12 16:58:26 +0100

    Adding warning when trying to install on python<2.6

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-
+from __future__ import with_statement
 from distutils.core import setup
+import sys
+
+if sys.version_info < (2,6):
+    print("Error: Python 2.6 is required.")
+    sys.exit(1)
 
 with open('README.md') as readme:
     long_description = readme.read()
